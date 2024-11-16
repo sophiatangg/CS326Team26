0a544f8f936e57f0bad4363939c00417ff31a4fb
Anne-Colombe Sinkpon <asinkpon@umass.edu>
2024-11-16 15:22:27 -0500

    Users can edit their profile information. Still need to fix issues with pfp.

--- frontend/components/AppControllerComponent/AppControllerComponent.js
+++
@@ -1,9 +0,0 @@
-import { Home } from '../Home/Home.js';
-import { Events } from '../Events/Events.js';
-import { About } from '../About/About.js';
-import { Profile } from '../Profile/Profile.js';
-import { EventHub } from '../../eventhub/EventHub.js';
-
-export class AppControllerComponent {
-  
-}
