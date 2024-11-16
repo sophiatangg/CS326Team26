f2c9f4f7fbb0a0be6b3d234c750ca1bc56260efa
Anne-Colombe Sinkpon <asinkpon@umass.edu>
2024-11-15 21:58:10 -0500

    Created AppController

---
+++ frontend/components/AppControllerComponent/AppControllerComponent.js
@@ -0,0 +1,9 @@
+import { Home } from '../Home/Home.js';
+import { Events } from '../Events/Events.js';
+import { About } from '../About/About.js';
+import { Profile } from '../Profile/Profile.js';
+import { EventHub } from '../../eventhub/EventHub.js';
+
+export class AppControllerComponent {
+  
+}
