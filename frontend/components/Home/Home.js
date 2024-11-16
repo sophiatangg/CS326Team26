20def34e2003ebee8119a054d4794bd9f58faf29
Mehek Shah <mehekshah20004@gmail.com>
2024-11-16 01:27:29 -0500

    landing page and overall UI

--- frontend/components/Home/Home.js
+++ frontend/components/Home/Home.js
@@ -9,16 +9,37 @@ export class Home extends BaseComponent {
     render() {
         const container = document.createElement('section');
         container.id = 'home-view';
+        const header = document.createElement('h1');
+        header.textContent = 'OpenInvite';
+        header.classList.add("openInvite")
+        const mainDiv = document.createElement("div");
+        
+        const motto = document.createElement("p");
+        motto.classList.add("motto");
+        motto.innerHTML="Connect, Share, Belong"
 
-        const header = document.createElement('h2');
-        header.textContent = 'Welcome to OpenInvite';
-
-        const description = document.createElement('p');
-        description.textContent = 'Find events in your community and engage with others.';
-
+        mainDiv.appendChild(motto);
         container.appendChild(header);
-        container.appendChild(description);
+        container.appendChild(mainDiv);
+
+        const signUp = document.createElement("a");
+        signUp.innerHTML = "Sign Up";
+        signUp.classList.add("btn-primary")
+        const explore = document.createElement("a");
+        explore.innerHTML = "Explore Events";
+        explore.classList.add("btn-secondary")
+        const btnDiv = document.createElement("div");
+        btnDiv.classList.add("cta-buttons")
+        btnDiv.appendChild(signUp);
+        btnDiv.appendChild(explore);
+        container.appendChild(btnDiv);
+        
+        const docHeader = document.getElementById("viewContainer");
+        docHeader.appendChild(container);
 
         return container;
     }
 }
+
+
+
