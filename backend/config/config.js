06f272e6e94e1c837f25b96b952cca60b5b305c4
Mason Choi <choi.mason@gmail.com>
2024-12-09 16:28:27 -0500

    Initialize sqlite with seeder data and document backend directory

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,16 +1,16 @@
 require('dotenv').config();
 
 module.exports = {
-    development: {
-        dialect: 'sqlite',
-        storage: 'openinvite.db',
-    },
-    test: {
-        dialect: 'sqlite',
-        storage: ':memory:',
-    },
-    production: {
-        dialect: 'sqlite',
-        storage: 'openinvite.db',
-    },
+  development: {
+    dialect: 'sqlite',
+    storage: 'openinvite.db', // SQLite database file
+  },
+  test: {
+    dialect: 'sqlite',
+    storage: ':memory:',
+  },
+  production: {
+    dialect: 'sqlite',
+    storage: 'openinvite.db',
+  },
 };
