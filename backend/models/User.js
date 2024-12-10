7773e12f4381f75ae889bd6397b1d3828b86a907
Anne-Colombe Sinkpon <asinkpon@umass.edu>
2024-12-09 23:09:44 -0500

    <User Interaction><FEATURE: User Interaction> updated user model to add id field as primary key and make it autoincrement, added email validation to email field, added picture name and mime so I could use base64 function to convert profile picture file, created a belongstomany relationship between followers and following and associated that to User model.

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,7 +1,15 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
+const UserFollowers = require('./Followers'); // Import UserFollowers table
+
+
 
 const User = sequelize.define('User', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
     username: { 
         type: DataTypes.STRING,
         unique: true,
@@ -13,7 +21,9 @@ const User = sequelize.define('User', {
         unique: true,
         validate: {
             // checks Email format 
-            isEmail: true,
+            isEmail: { 
+                msg: 'Must be a valid email address' 
+            },
          }
     },
         
@@ -22,19 +32,54 @@ const User = sequelize.define('User', {
         allowNull: false },
     profile_picture: { 
         type: DataTypes.STRING,
-        defaultValue: "",
+        allowNull: true,
      },
     bio: { 
-        type: DataTypes.TEXT },
-    followers: { 
-        type: DataTypes.INTEGER, 
-        defaultValue: 0 },
-    following: { 
-        type: DataTypes.INTEGER, 
-        defaultValue: 0 },
-    role: { 
-        type: DataTypes.STRING, 
-        defaultValue: "user" },
+        type: DataTypes.TEXT 
+    },
+    picturename: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+    mime: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+    },
+);
+
+/*
+Finding "followers" in openonvite.db (users who follow a specific user):
+Use the foreignKey: 'userId'.
+Look for rows in UserFollowers where userId = the target user's ID.
+
+
+Finding "following" in openonvite.db (users a specific user follows):
+Use the foreignKey: 'followerId'.
+Look for rows in UserFollowers where followerId = the target user's ID.
+*/
+// user can follow and be followed by other users.
+User.belongsToMany(User, {
+    // Alias for how the follower relationship is referenced in application.
+    as: 'Followers', 
+    // Join table
+    through: UserFollowers, 
+    // Specifies the "followee"
+    foreignKey: 'userId', 
+    // Specifies the "Follower"
+    otherKey: 'followerId', 
+});
+
+User.belongsToMany(User, {
+    // alias for how the following relationship is referenced in application.
+    as: 'Following', 
+    //  join table that stores the connections between followers and those being followed.
+    through: UserFollowers, 
+    //In the UserFollowers table, links to the primary key (id) of the User model.
+    // Specifies the "follower"
+    foreignKey: 'followerId',
+    // Specifies the "followee"
+    otherKey: 'userId', 
 });
 
 module.exports = User;
